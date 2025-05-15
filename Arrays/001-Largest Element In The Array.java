f7b0a7f897f01128f1e67e3e8c6a322c4fea768b
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-05-15 12:43:02 +0530

    Update 001-Largest Element In The Array.java

--- Arrays/001-Largest Element In The Array.java
+++ Arrays/001-Largest Element In The Array.java
@@ -1,1 +1,18 @@
 
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+        int maxi=arr[0];
+        
+        for( int i=1;i<arr.length;i++)
+        {
+            if(arr[i]>maxi)
+            {
+                maxi=arr[i];
+            }
+        }
+        return maxi;
+        
+    }
+
+
