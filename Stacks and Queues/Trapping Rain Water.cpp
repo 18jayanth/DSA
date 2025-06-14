1d1d8503c9bc6eeb6ac52502502e44767360184b
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-06-14 15:23:28 +0530

    Create Trapping Rain Water.cpp

---
+++ Stacks and Queues/Trapping Rain Water.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int left=0;
+        int right=n-1;
+        int ans=0;
+        int leftmax=-1;
+        int rightmax=-1;
+      //traverse from left to right using two pointers
+        while(left<=right)
+        {
+          //base condition total=total +min(leftmax,rightmax)-height[i]
+          //if height[left] is smaller than right then left  may hold some water
+          //if it is less than left max only then it can hold water or update the left max
+            if(height[left]<=height[right])
+            {
+                if(leftmax<=height[left])
+                leftmax=height[left];
+                else
+                ans+=leftmax-height[left];
+                left++;
+
+            }
+           //if height[right] is smaller than left then right  may hold some water
+          //if it is less than right  max only then it can hold water or update the right max
+            else
+            {
+                if(rightmax<=height[right])
+                rightmax=height[right];
+                else
+                ans+=rightmax-height[right];
+                right--;
+
+            
+            }
+
+        }
+        return ans;
+        
+    }
+};
