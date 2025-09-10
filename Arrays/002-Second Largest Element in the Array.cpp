294b119ecd26ba5fe3f1db67d14aa5ef7d7b08b9
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-10 15:35:49 +0530

    Update 002-Second Largest Element in the Array.cpp

--- Arrays/002-Second Largest Element in the Array.cpp
+++ Arrays/002-Second Largest Element in the Array.cpp
@@ -1,1 +1,21 @@
-
+** Take Away 1)Should not use equal symbol while comparing  and largest number must also be -1 along with sec_largest according to requirement **
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int sec_largest=-1;
+        int large=-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>large)
+            {
+                sec_largest=large;
+                large=arr[i];
+            }
+            else if(arr[i]<large && arr[i]>sec_largest)
+            sec_largest=arr[i];
+        }
+        return sec_largest;
+        
+    }
+};
