6af37fbd60adbbbeaa7f7aefeb6247e1cb568c88
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-11 12:06:24 +0530

    Update 006-Left Rotate an array by k places.cpp

--- Arrays/006-Left Rotate an array by k places.cpp
+++ Arrays/006-Left Rotate an array by k places.cpp
@@ -6,6 +6,7 @@ class Solution {
         // code here
         
         int n=arr.size();
+      // or vector<int>temp(n); will give same result
         int temp[n];
         int j=0;
       //very important d always should be less than n
@@ -26,5 +27,34 @@ class Solution {
             arr[i]=temp[j++];
             
         }
-        
     }
+      // Another Optimal way to reverse the 1st part REverse the second part and reverse string as  a Whole
+//This approach is better as it wont required extra array
+
+class Solution {
+  public:
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void reverse(vector<int>&arr,int start,int end)
+        {
+            while(start<end)
+            {
+                swap(arr[start],arr[end]);
+                start++;
+                end--;
+            }
+        }
+    void rotateArr(vector<int>& arr, int d) {
+        
+        // code here
+        int n=arr.size();
+         d=d%n;
+        if(d<=0)return ;
+       
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+};
+        
+    
