8c4f8530251c917e50fab1513c071a060f63de77
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-27 02:47:53 +0530

    Create Diameter of a binary tree.cpp

---
+++ Trees/Diameter of a binary tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int maxdepth(TreeNode*root,int &maxi)
+{
+    if(root==NULL) return 0;
+    int lh=maxdepth(root->left,maxi);
+    int rh=maxdepth(root->right,maxi);
+    maxi=max(maxi,lh+rh);
+    return 1+max(lh,rh);
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter=0;
+        maxdepth(root,diameter);
+        return diameter;
+        
+    }
+};
