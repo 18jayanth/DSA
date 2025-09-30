44c6d9c8874f48d3841e2f2e558a2a66958f527c
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-30 21:10:18 +0530

    Update 004-Remove Duplicates from sorted array.cpp

--- Arrays/004-Remove Duplicates from sorted array.cpp
+++ Arrays/004-Remove Duplicates from sorted array.cpp
@@ -1,1 +1,14 @@
-
+ vector<int> removeDuplicates(vector<int> &arr) {
+        // code here
+        vector<int>ans;
+        int last=-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]!=last)
+            {
+            ans.push_back(arr[i]);
+            last=arr[i];
+             }
+       }
+     return ans;
+    }
