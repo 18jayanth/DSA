7ea02ec973ee68f5ba8abb29d0303578faf598fc
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-10-17 15:12:17 +0530

    Create 006-Kth smallest element in Binary Search Tree.cpp

---
+++ Trees/Binary Search Trees/006-Kth smallest element in Binary Search Tree.cpp
@@ -0,0 +1,26 @@
+void  inorder(TreeNode* root,int k,int& cnt,int& value)
+{
+    if(!root || cnt>=k) return  ;
+     inorder(root->left,k,cnt,value);
+    cnt++;
+    if(cnt==k)
+    {
+        value=root->val;
+        return;
+    }
+
+     inorder(root->right,k,cnt,value);
+}
+    int kthSmallest(TreeNode* root, int k) {
+    int cnt=0;
+    if(!root) return -1;
+    
+    int val=-1;
+    inorder(root,k,cnt,val);
+    return val;
+
+
+        
+        
+    }
+};
